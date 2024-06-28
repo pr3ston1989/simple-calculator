d5d8b2c07d8bdf0c805eabf39397f7374140727f
Damian Pietrzak <{ID}+{username}@users.noreply.github.com>
2024-06-28 21:23:19 +0200

    Refactor most of code to functions.

--- script.js
+++ script.js
@@ -37,32 +37,85 @@ let secondNumber = '';
 let operator = '';
 
 
-function clearScreen() {
-    firstNumber = '';
-    secondNumber = '';
-    operator = '';
-    mainLabel.textContent = '';
-    secondaryLabel.textContent = '';
+function clearScreen(button) {
+    if (button.textContent == "C") {
+        firstNumber = '';
+        secondNumber = '';
+        operator = '';
+        mainLabel.textContent = '';
+        secondaryLabel.textContent = '';
+    }
 }
 
-allButtons.forEach(button => button.addEventListener('click', () => {
+function populateScreen(button) {
+    mainLabel.textContent += button.textContent;
+}
+
+function calculate(button) {
     if (button.className == 'digits') {
         mainLabel.textContent += button.textContent;
-        secondNumber += button.textContent;
     } else if (button.className == 'action') {
-        firstNumber = secondNumber;
-        secondNumber = '';
+        if (operator == '' && button.textContent != '=') {
+            firstNumber = mainLabel.textContent;
+            operator = button.textContent;
+            secondaryLabel.textContent = `${firstNumber} ${operator}`
+            mainLabel.textContent = ''
+        } else if (button.textContent == '=' && operator != '') {
+            secondNumber = mainLabel.textContent;
+            let result = operate(firstNumber, secondNumber, operator)
+            secondaryLabel.textContent = result;
+            firstNumber = result;
+            mainLabel.textContent = '';
+        } else if (operator != '' && mainLabel.textContent == '') {
+            operator = button.textContent;
+        } else {
+            if (mainLabel.textContent != '') {
+                secondNumber = mainLabel.textContent;
+                secondaryLabel.textContent = operate(firstNumber, secondNumber, operator);
+                firstNumber = secondaryLabel.textContent;
+                operator = button.textContent;
+                mainLabel.textContent = '';
+            }
+        }
+    }
+}
+
+
+allButtons.forEach(button => button.addEventListener('click', () => {
+/*    if (button.className == 'digits') {
+        mainLabel.textContent += button.textContent;
+    } else if (button.className == 'action' && secondNumber == '' && operator != '') {
+        operator = button.textContent;
+        let labelText = secondaryLabel.textContent.split(' ');
+        secondaryLabel.textContent = `${labelText[0]} ${operator}`;
+    } else if (button.className == 'action' && operator == '') {
+        firstNumber = mainLabel.textContent;
         operator = button.textContent;
         secondaryLabel.textContent = `${firstNumber} ${operator}`;
         mainLabel.textContent = '';
+    } else if (button.className == 'action' && secondNumber == '') {
+        secondNumber = mainLabel.textContent;
+        let temp = operate(firstNumber, secondNumber, operator);
+        secondaryLabel.textContent = `${temp} ${operator}`;
+        firstNumber = temp;
+        mainLabel.textContent = '';
+        operator = button.textContent;
+    } else if (button.className == 'action' && secondNumber != '') {
+        let temp = operate(firstNumber, secondNumber, operator);
+        secondaryLabel.textContent = `${temp} ${operator}`;
+        operator = button.textContent;
+        firstNumber = temp;
+        secondNumber = '';
     } else if (button.className == 'equals') {
+        secondNumber = mainLabel.textContent;
         mainLabel.textContent = operate(firstNumber, secondNumber, operator);
         secondaryLabel.textContent += ` ${secondNumber} =`;
-        secondNumber = mainLabel.textContent;
+        secondNumber = '';
     } else if (button.textContent == "C") {
         clearScreen();
     }
-    console.log(firstNumber);
-    console.log(secondNumber);
-    console.log(operator);
+*/
+
+    calculate(button);
+    clearScreen(button);
 }));
