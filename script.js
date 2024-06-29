f2fb3cc60cce0828058f63d01416e6da445ba0d4
Damian Pietrzak <{ID}+{username}@users.noreply.github.com>
2024-06-29 21:37:28 +0200

    Repair bugs and make small visual changes.

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 function operate(numOne, numTwo, operator) {
     const one = parseFloat(numOne);
     const two = parseFloat(numTwo);
+    let result = 0;
     if (operator == "+") {
-        return sum(one, two);
+        result = sum(one, two);
     } else if (operator == "-") {
-        return substract(one,two);
+        result = substract(one,two);
     } else if (operator == "x") {
-        return multiply(one, two);
+        result = multiply(one, two);
     } else if (operator == "÷") {
-        return divide(one, two);
+        result = divide(one, two);
+    }
+    if (result == Math.floor(result)) {
+        if (String(result).length > 11) {
+            return result.toExponential(5);
+        }
+        return result;
+    } else {
+        let resultBaseLength = String(Math.round(result)).length;
+        if (result.toFixed(6).length > 11) {
+            return result.toExponential(5);
+        } else {
+            return result.toFixed(10 - resultBaseLength);
+        }
     }
 }
 
@@ -25,16 +39,20 @@ function multiply(numOne, numTwo) {
 }
 
 function divide(numOne, numTwo) {
+    if (numTwo == 0) {
+        return "ERROR";
+    }
     return numOne / numTwo;
 }
 
-const secondaryLabel = document.querySelector("#equation");
 const mainLabel = document.querySelector("#result");
 const allButtons = document.querySelectorAll("button");
+const commaButton = document.querySelector("#comma");
 
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
+let actionFlag = true;
 
 
 function clearScreen(button) {
@@ -42,80 +60,79 @@ function clearScreen(button) {
         firstNumber = '';
         secondNumber = '';
         operator = '';
-        mainLabel.textContent = '';
-        secondaryLabel.textContent = '';
+        mainLabel.textContent = '0';
+        actionFlag = true;
     }
-}
-
-function populateScreen(button) {
-    mainLabel.textContent += button.textContent;
 }
 
 function calculate(button) {
     if (button.className == 'digits') {
-        mainLabel.textContent += button.textContent;
+        if (actionFlag == true && button.id == 'comma') {
+            mainLabel.textContent = "0.";
+            button.disabled = true;
+            actionFlag = false;
+        } else if (actionFlag == true) {
+            mainLabel.textContent = button.textContent;
+            actionFlag = false;
+        } else {
+            if (mainLabel.textContent.length < 11) {
+                if (button.id == 'comma') {
+                    button.disabled = true;
+                }
+                mainLabel.textContent += button.textContent;
+            }
+        }
     } else if (button.className == 'action') {
-        if (operator == '' && button.textContent != '=') {
+        commaButton.disabled = false;
+        if (actionFlag == false && firstNumber == '') {
+            operator = button.textContent;
+            firstNumber = mainLabel.textContent;
+            actionFlag = true;
+        } else if (actionFlag == false && firstNumber != '' && secondNumber == '') {
+            if (operator == '') {
+                operator = button.textContent;
+            }
+            secondNumber = mainLabel.textContent;
+            mainLabel.textContent = operate(firstNumber, secondNumber, operator);
+            operator = button.textContent;
+            firstNumber = mainLabel.textContent;
+            secondNumber = '';
+            actionFlag = true;  
+        } else if (actionFlag == true && firstNumber == '') {
             firstNumber = mainLabel.textContent;
             operator = button.textContent;
-            secondaryLabel.textContent = `${firstNumber} ${operator}`
-            mainLabel.textContent = ''
-        } else if (button.textContent == '=' && operator != '') {
-            secondNumber = mainLabel.textContent;
-            let result = operate(firstNumber, secondNumber, operator)
-            secondaryLabel.textContent = result;
-            firstNumber = result;
-            mainLabel.textContent = '';
-        } else if (operator != '' && mainLabel.textContent == '') {
-            operator = button.textContent;
-        } else {
-            if (mainLabel.textContent != '') {
+        }
+    } else if (button.className == 'equals') {
+        commaButton.disabled = false;
+        if (firstNumber != '' && secondNumber != '' && operator != '') {
+            mainLabel.textContent = operate(firstNumber, secondNumber, operator);
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+            actionFlag = true;
+        } else if (firstNumber != '' && secondNumber == '' && operator != '') {
+            if (mainLabel.textContent != firstNumber) {
                 secondNumber = mainLabel.textContent;
-                secondaryLabel.textContent = operate(firstNumber, secondNumber, operator);
-                firstNumber = secondaryLabel.textContent;
-                operator = button.textContent;
-                mainLabel.textContent = '';
+            } else {
+                secondNumber = firstNumber;
             }
+            mainLabel.textContent = operate(firstNumber, secondNumber, operator);
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+            actionFlag = true;
+        }
+    } else if (button.id == 'erase' && actionFlag == false) {
+        if (mainLabel.textContent.length > 1) {
+            mainLabel.textContent = mainLabel.textContent.slice(0, -1);
+        } else {
+            mainLabel.textContent = '0';
+            actionFlag = true;
         }
     }
 }
 
-
 allButtons.forEach(button => button.addEventListener('click', () => {
-/*    if (button.className == 'digits') {
-        mainLabel.textContent += button.textContent;
-    } else if (button.className == 'action' && secondNumber == '' && operator != '') {
-        operator = button.textContent;
-        let labelText = secondaryLabel.textContent.split(' ');
-        secondaryLabel.textContent = `${labelText[0]} ${operator}`;
-    } else if (button.className == 'action' && operator == '') {
-        firstNumber = mainLabel.textContent;
-        operator = button.textContent;
-        secondaryLabel.textContent = `${firstNumber} ${operator}`;
-        mainLabel.textContent = '';
-    } else if (button.className == 'action' && secondNumber == '') {
-        secondNumber = mainLabel.textContent;
-        let temp = operate(firstNumber, secondNumber, operator);
-        secondaryLabel.textContent = `${temp} ${operator}`;
-        firstNumber = temp;
-        mainLabel.textContent = '';
-        operator = button.textContent;
-    } else if (button.className == 'action' && secondNumber != '') {
-        let temp = operate(firstNumber, secondNumber, operator);
-        secondaryLabel.textContent = `${temp} ${operator}`;
-        operator = button.textContent;
-        firstNumber = temp;
-        secondNumber = '';
-    } else if (button.className == 'equals') {
-        secondNumber = mainLabel.textContent;
-        mainLabel.textContent = operate(firstNumber, secondNumber, operator);
-        secondaryLabel.textContent += ` ${secondNumber} =`;
-        secondNumber = '';
-    } else if (button.textContent == "C") {
-        clearScreen();
-    }
-*/
-
     calculate(button);
     clearScreen(button);
 }));
